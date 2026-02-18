b330ad3c746ae4a6c8369e064706e48f765d79da
Olatunji Peter <polalekan526@gmail.com>
2026-02-18 19:06:16 +0100

    landing page hero section done

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,6 +1,26 @@
 import { Globe } from "lucide-react";
+import { motion } from 'framer-motion';
 
 const Hero = () => {
+
+    const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2, 
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { 
+      opacity: 1, 
+      y: 0,
+      transition: { duration: 0.5 } 
+    },
+  };
   return (
     <div className="mt-18">
       <div className="relative h-screen w-full flex items-center justify-center">
@@ -14,28 +34,68 @@ const Hero = () => {
 
         <div className="relative z-10 text-center">
           <div className="w-full justify-center items-center flex">
-            <div className="bg-gray-50/30 flex rounded-full items-center  justify-between px-6 py-[6px]">
+            <div className="bg-gray-50/20 flex rounded-full items-center font-bold justify-between px-6 py-[6px]">
               <Globe className="text-[#E80F1A] mr-2" />
               <h4 className="lato text-gray-100 text-[12px]"> OUR VISION</h4>
             </div>
           </div>
-          <h1 className="text-6xl font-bold text-white  my-8">
-            We Evangelize the World to{" "}
-            <span className="text-[#E80F1A]">
-              Receive <br /> Christ
-            </span>{" "}
-            And Disciple the Church to <br />{" "}
-            <span className="text-[#E80F1A]">Reflect Christ.</span>
-          </h1>
+         <motion.h1 
+      className="lg:text-5xl text-3xl viga md:block hidden px-2  font-bold text-white my-8"
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible" 
+      viewport={{ once: true, amount: 0.5 }} 
+    >
+      {"We Evangelize the World to ".split(" ").map((word, i) => (
+        <motion.span key={i} variants={childVariants} className="inline-block mr-2">
+          {word}
+        </motion.span>
+      ))}
 
-          <div className="flex justify-center lora mt-20">
-            <h2 className="text-[15px] hover:bg-[#E80F1A] transition-colors duration-300 cursor-pointer hover:text-white tracking-[4px] mr-4 bg-white text-black rounded-full py-6 px-6">
-              WATCH A SERMON
-            </h2>
-            <h2 className="text-[15px] bg-Transparent tracking-[4px] hover:bg-[#E80F1A] hover:border-0 transition-colors duration-300 cursor-pointer hover:text-white  text-white border border-white rounded-full py-6 px-6">
-              REQUEST PRAYER
-            </h2>
-          </div>
+      <span className="text-[#E80F1A]">
+        <motion.span variants={childVariants} className="inline-block ">Receive</motion.span>
+        <br />
+        <motion.span variants={childVariants} className="inline-block">Christ</motion.span>
+      </span>
+
+      {" And Disciple the Church to ".split(" ").map((word, i) => (
+        <motion.span key={i + 10} variants={childVariants} className="inline-block mx-2">
+          {word}
+        </motion.span>
+      ))}
+
+      <br />
+
+      <span className="text-[#E80F1A]">
+        <motion.span variants={childVariants} className="inline-block">Reflect Christ.</motion.span>
+      </span>
+    </motion.h1>
+
+<motion.h1 
+variants={containerVariants}
+      initial="hidden"
+      whileInView="visible" 
+      viewport={{ once: true, amount: 0.5 }} 
+className="text-3xl md:hidden block viga font-bold text-white px-2  my-8">
+            We Evangelize the World to
+            <b className="text-[#E80F1A]"> {" "}
+              Receive  Christ
+            </b>And Disciple the Church to 
+            <b className="text-[#E80F1A]"> {" "}Reflect Christ.</b> 
+          </motion.h1>
+
+
+         <div className="flex flex-col items-center justify-center md:flex-row lora mt-20 gap-4">
+  
+  <h2 className="text-[15px] flex items-center justify-center hover:bg-[#E80F1A] transition-colors duration-300 cursor-pointer hover:text-white tracking-[3px] bg-white text-black rounded-full md:w-60 w-50  text-center py-4">
+    WATCH A SERMON
+  </h2>
+
+  <h2 className="text-[15px] flex items-center justify-center bg-transparent tracking-[3px] hover:bg-[#E80F1A] hover:border-0 transition-colors duration-300 cursor-pointer text-white border border-white rounded-full md:w-60 w-50 py-4">
+    REQUEST PRAYER
+  </h2>
+  
+</div>
         </div>
       </div>
     </div>
