2271ca083bb0194356c6331c68b3d05feb0a3e89
Olatunji Peter <polalekan526@gmail.com>
2026-02-20 14:26:25 +0100

    reading problem solved

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -2,17 +2,6 @@ import { Globe } from "lucide-react";
 import { motion } from 'framer-motion';
 
 const Hero = () => {
-
-    const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2, 
-      },
-    },
-  };
-
   const childVariants = {
     hidden: { opacity: 0, y: 20 },
     visible: { 
