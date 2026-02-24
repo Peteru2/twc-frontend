70a7455237e43e0527f6fdbed69adf5b5670f449
Olatunji Peter <polalekan526@gmail.com>
2026-02-24 18:02:44 +0100

    Errors removed

--- src/components/sermons/Hero.tsx
+++ src/components/sermons/Hero.tsx
@@ -1,26 +1,10 @@
-import { Globe } from "lucide-react";
+
 import { motion } from 'framer-motion';
 
 const Hero = () => {
 
-    const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2, 
-      },
-    },
-  };
 
-//   const childVariants = {
-//     hidden: { opacity: 0, y: 20 },
-//     visible: { 
-//       opacity: 1, 
-//       y: 0,
-//       transition: { duration: 0.5 } 
-//     },
-//   };
+
   return (
     <div className="">
       <motion.div 
