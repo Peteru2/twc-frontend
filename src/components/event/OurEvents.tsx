538cb5123b25a64d9fbbd94859e670605e4bc6b4
Olatunji Peter <polalekan526@gmail.com>
2026-02-24 19:58:23 +0100

    Events page updated

--- src/components/event/OurEvents.tsx
+++ src/components/event/OurEvents.tsx
@@ -8,10 +8,10 @@ export const OurEvents = () => {
          <div  className="flex justify-center  mt-16 w-full">
             <div>
          <h2 className="md:text-2xl text-xl font-bold text-center viga tracking-wide">
-        UPCOMING <span className="text-red-600">EVENTS</span>
+        OUR <span className="text-red-600">EVENTS</span>
       </h2>
       <div>
-        <img src="/line.png" className="mt-2  w-60 md:w-80 origin-center" />
+        <img src="/line.png" className="mt-2  w-40 md:w-60 origin-center" />
       </div>
       </div>
     
